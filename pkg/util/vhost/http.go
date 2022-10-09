ed30af2c75739bb970bd2afac8e57371db69706a
ziv-codefresh <ziv@codefresh.io>
2022-10-09 15:01:47 +0300

    wip

--- pkg/util/vhost/http.go
+++ pkg/util/vhost/http.go
@@ -188,23 +188,18 @@ func (rp *HTTPReverseProxy) CheckAuth(domain, location, routeByHTTPUser, user, p
 // CheckClientOriginIpAddr to prevent IP spoofing, be sure to delete any pre-existing X-Forwarded-For header coming from the client or an untrusted proxy.
 func (rp *HTTPReverseProxy) CheckClientOriginIpAddr(domain, location, routeByHTTPUser, addr string) bool {
 	if addr != "" {
-		frpLog.Debug("Received client ip addr list: %s", addr)
+		frpLog.Debug("Received client ip addr: %s", addr)
 		ips := strings.Split(addr, ", ")
 		if len(ips) > 1 {
 			// Selecting the first ip in the list, it's safe to take it once we ensured the first ip cannot be set by untrusted proxies or the client
 			addr = ips[0]
 		}
 	}
-	host, _, err := net.SplitHostPort(addr)
-	if err != nil {
-		frpLog.Error("address is invalid %s", err)
-		return false
-	}
 	vr, ok := rp.getVhost(domain, location, routeByHTTPUser)
 	if ok {
 		if vr.ipFilter != nil {
-			frpLog.Debug("validating client origin ip %s", host)
-			return vr.ipFilter.Allowed(host)
+			frpLog.Debug("validating client origin ip %s", addr)
+			return vr.ipFilter.Allowed(addr)
 		}
 	}
 	return true
@@ -321,8 +316,8 @@ func (rp *HTTPReverseProxy) ServeHTTP(rw http.ResponseWriter, req *http.Request)
 	// Identifying the originating IP address of a client connecting to a web server through a proxy server
 	addr := req.Header.Get("X-Forwarded-For")
 	if addr == "" {
-		// For server direct access
-		addr = req.RemoteAddr
+		// For server direct access, remote address is in "IP:port" format
+		addr, _, _ = net.SplitHostPort(req.RemoteAddr)
 	}
 	if !rp.CheckClientOriginIpAddr(domain, location, user, addr) {
 		http.Error(rw, http.StatusText(http.StatusForbidden), http.StatusForbidden)
